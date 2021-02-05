f698f25eaf5852a8570586037178b4751b155fcd
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-02-05 13:56:06 -0600

    started work on version 1, added new routes for house status

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 });
 
+app.post("/smart-house/status", (req, res) => {
+  console.log("POST smart house checker route");
+});
+
+app.get("/smart-house/status/:id", (req, res) => {
+  console.log("smart house checker route");
+});
+
 app.get("/heartbeat", (req, res) => {
   res.send({
     heartbeat: "badum badum badum",
